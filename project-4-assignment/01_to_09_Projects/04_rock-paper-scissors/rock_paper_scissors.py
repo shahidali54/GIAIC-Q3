601ed6feec6b5d300a2b59ebde9bd181c916cb03
Shahid Ali <153486921+shahidali54@users.noreply.github.com>
2025-06-14 09:52:35 +0500

    Update rock_paper_scissors.py

--- project-4-assignment/01_to_09_Projects/04_rock-paper-scissors/rock_paper_scissors.py
+++ project-4-assignment/01_to_09_Projects/04_rock-paper-scissors/rock_paper_scissors.py
@@ -19,10 +19,3 @@ def is_win(player, opponent):
         return True
  
 print(play())
-# This code implements a simple rock-paper-scissors game where the user plays against the computer.
-# The user inputs their choice, and the computer randomly selects one of the three options.
-
-# The game determines the winner based on the rules of rock-paper-scissors.
-# If both choices are the same, it results in a tie.
-# If the user's choice beats the computer's choice, the user wins; otherwise, the user loses.
-# The `is_win` function checks if the player's choice beats the opponent's choice.
